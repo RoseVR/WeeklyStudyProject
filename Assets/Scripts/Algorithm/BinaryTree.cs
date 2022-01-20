b8d31da0d4d73672005eaa9dd1dd52f74182e130
RoseVR <501625014@qq.com>
2022-01-20 21:11:41 +0800

    【a】二叉树

--- Assets/Scripts/Algorithm/BinaryTree.cs
+++ Assets/Scripts/Algorithm/BinaryTree.cs
@@ -22,16 +22,52 @@ public class BinaryTree : MonoBehaviour
     void Start()
     {
         Array = new int[] { 10, 6, 7, 4, 8, 2, 1 };
+        for(int i = 0; i < Array.Length; i++)
+        {
+            InsertNode(i, Array);
+        }
     }
-    private void InsertNode(int num,int index,int[] array)
+    private void InsertNode(int index,int[] array)
     {
         //根节点
         if (index == 0)
         {
-            Node<int> root = new Node<int>(num);
+            Node<int> root = new Node<int>(array[index]);
             root.parentNode = null;
-            root.leftChild = new Node<int>(array[index + 1]);
-            root.rightChild= new Node<int>(array[index + 2]);
+            if (index + 1 < array.Length)
+            {
+                root.leftChild = new Node<int>(array[index + 1]);
+                root.leftChild.parentNode = root;
+            }
+            else
+                root.leftChild = null;
+            if (index + 2 < array.Length)
+            {
+                root.rightChild = new Node<int>(array[index + 2]);
+                root.rightChild.parentNode = root;
+            }
+            else
+                root.rightChild = null;
+            Debug.Log(" root = " + root.data + "root.left = " + root.leftChild.data + "right=" + root.rightChild.data);
+        }
+        else
+        {
+            Node<int> parent = new Node<int>(array[index]);
+            if (index + 1 < array.Length)
+            {
+                parent.leftChild = new Node<int>(array[index + 1]);
+                parent.leftChild.parentNode = parent;
+            }
+            else
+                parent.leftChild = null;
+            if (index + 2 < array.Length)
+            {
+                parent.rightChild = new Node<int>(array[index + 2]);
+                parent.rightChild.parentNode = parent;
+            }
+            else
+                parent.rightChild = null;
+            Debug.Log(" parent = " + parent.data + "root.left = " + parent.leftChild.data + "right=" + parent.rightChild.data);
         }
     }
     // Update is called once per frame
